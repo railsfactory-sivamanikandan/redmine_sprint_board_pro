ff21d42148035337c86af89bfdb0b7379da698bf
sivamanikandan <sivamanikandan@railsfactory.org>
2025-08-20 12:58:34 +0530

    revert the condition check

--- db/migrate/20250811082208_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
+++ db/migrate/20250811082208_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
@@ -18,7 +18,7 @@ class ActsAsTaggableOnMigration < ActiveRecord::Migration[6.0]
 
       # Limit is created to prevent MySQL error on index
       # length for MyISAM table type: http://bit.ly/vgW2Ql
-      t.string :context, limit: 128 unless column_exists?(ActsAsTaggableOn.taggings_table, :context)
+      t.string :context, limit: 128
 
       t.datetime :created_at
     end
