d7abcae8ba5978560501b87f8cabb90481c28392
sivamanikandan <sivamanikandan@railsfactory.org>
2025-09-01 18:37:07 +0530

    fixed file load issue

--- lib/redmine_sprint_board_pro/project_agile_query_extension.rb
+++ lib/redmine_sprint_board_pro/project_agile_query_extension.rb
@@ -1,11 +1,13 @@
-module ProjectAgileQueryExtension
-  def self.included(base)
-    base.class_eval do
-      has_many :agile_queries, -> { where(type: 'AgileQuery') }, 
-               class_name: 'AgileQuery', 
-               dependent: :delete_all
+module RedmineSprintBoardPro
+  module ProjectAgileQueryExtension
+    def self.included(base)
+      base.class_eval do
+        has_many :agile_queries, -> { where(type: 'AgileQuery') },
+                class_name: 'AgileQuery',
+                dependent: :delete_all
+      end
     end
   end
 end
 
-Project.include(ProjectAgileQueryExtension) unless Project.included_modules.include?(ProjectAgileQueryExtension)
+Project.include(RedmineSprintBoardPro::ProjectAgileQueryExtension) unless Project.included_modules.include?(RedmineSprintBoardPro::ProjectAgileQueryExtension)
